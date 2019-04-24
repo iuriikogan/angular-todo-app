edd102b9c315fe5079a2f8e687344a0fbe599748
Iurii Kogan <koganiurii@gmail.com>
2019-04-24 15:52:59 +0700

    backend integrated

---
+++ models/task.js
@@ -0,0 +1,19 @@
+// Import db.js
+const db = require('../db')
+// Import mongoose from node_modules
+const mongoose = require('mongoose')
+
+// Create schema for task
+const db_task = db.model('task', {
+	__v: {
+		type: Number,
+		select: false
+	},
+	name: {
+		type: String,
+		required: true
+	}
+})
+
+// Export
+module.exports = db_task
