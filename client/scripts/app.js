548dbc1c90373fb2654cc0db479f67d72e809d67
Iurii Kogan <koganiurii@gmail.com>
2019-04-24 15:55:24 +0700

    app.js updated

---
+++ client/scripts/app.js
@@ -0,0 +1,51 @@
+    //config    
+const tasks_app = angular.module('tasks_app', [])
+var task
+    // ##tasks controller##
+tasks_app.controller('tasks', ['$scope', '$http', function($scope, $http) {
+    // ##get all tasks
+    const get_tasks = function() {
+        $http({
+            method: GET,
+            url:'/api/tasks'
+        })
+    }.then((function(res){
+        $scope.tasks = res.tasks    
+    },function(err){
+        console.log('err', err);
+    }))
+    //add a task
+    add_task = function() {
+        $http({
+            method: POST,
+            url:'/api/tasks',
+            data: {
+                name: $scope.task
+            }
+
+        })
+    }.then((function(req){
+        $scope.tasks = req.tasks
+        return $scope.tasks    
+    },function(err){
+        console.log('err', err);
+    }))
+    // delete_task
+    $scope.delete_task = function(selected_task) {
+		$http({
+			method: 'DELETE',
+			url: '/api/messages/' + selected_task.name
+		}).then(function(res) {
+			get_tasks()
+		}, function(err) {
+			console.log('err', err)
+		})
+    }
+    $scope.select_task = function(task) {
+		return task = $scope.selected_task 
+	}
+
+
+
+
+}])
