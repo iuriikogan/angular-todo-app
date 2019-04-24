4ba595fb17ec10989374e883db7876ab862ff5cc
Iurii Kogan <koganiurii@gmail.com>
2019-04-24 20:43:15 +0700

    add and get all tasks working

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,53 +1,74 @@
-    //config    
+import {
+    runInNewContext
+} from "vm";
+
+//config    
 const tasks_app = angular.module('tasks_app', [])
-var task
-    // ##tasks controller##
-tasks_app.controller('tasks', ['$scope', '$http', function($scope, $http) {
+// ##tasks controller##
+tasks_app.controller('tasks', ['$scope', '$http', function ($scope, $http) {
     // ##get all tasks
-    const get_tasks = function() {
+    const get_tasks = function () {
         $http({
             method: GET,
-            url:'/api/tasks'
+            url: '/api/tasks'
         })
-    }.then((function(res){
-        $scope.tasks = res.tasks    
-    },function(err){
+    }.then((function (res) {
+        $scope.tasks = res.data
+    }, function (err) {
         console.log('err', err);
     }))
+
+
     //add a task
-    add_task = function() {
+    add_task = function () {
         $http({
             method: POST,
-            url:'/api/tasks',
+            url: '/api/tasks',
             data: {
-                name: $scope.task
+                name: task.name
             }
-
         })
-    }.then((function(req){
-        $scope.tasks = req.tasks
-        $scope.tasks = ''
-        return $scope.tasks    
-        
-    },function(err){
+    }.then((function (res) {
+        $scope.task.push(res.task)
+        console.log("TCL: add_task -> req.task", req.task)
+        $scope.task = ''
+        return $scope.task
+    }, function (err) {
         console.log('err', err);
     }))
-    // delete_task
-    $scope.delete_task = function(selected_task) {
-		$http({
-			method: 'DELETE',
-			url: '/api/messages/' + selected_task.name
-		}).then(function(res) {
-			get_tasks()
-		}, function(err) {
-			console.log('err', err)
-		})
-    }
+
+    // selected_task
     $scope.select_task = function(task) {
-		return task = $scope.selected_task 
+		$scope.selected_task = task
 	}
 
 
+    // delete_task
+    $scope.delete_task = function () {
+        $http({
+            method: 'DELETE',
+            url: '/api/tasks/' + $scope.selected_task._id,
+        }).then(function (res) {
+            get_tasks()
+            console.log("TCL: $scope.delete_task -> $Scope.selected_task", $Scope.selected_task)
+            res.status(200)
+        }, function (err) {
+            console.log('err', err)
+        })
+    }
+    //complete a task
+    $scope.complete_task = function (selected_task) {
+        $http({
 
+        }).then(function (req) {
+            $scope.tasks.filter(function (tasks) {
+                get_tasks()
+                return req.body.task
+            }).then(function (err) {
+                console.log('err', err);
+            })
+
+        })
+    }
 
 }])
