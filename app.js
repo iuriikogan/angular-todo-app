f344e35ae4e296b201cd46dc632bdc3b20753b3f
Iurii Kogan <koganiurii@gmail.com>
2019-04-26 09:03:13 +0700

    tasks loading into ui

--- app.js
+++ app.js
@@ -4,77 +4,73 @@ const tasks_app = angular.module('tasks_app', [])
 tasks_app.controller('tasks', ['$scope', '$http', function ($scope, $http) {
     console.log('this is working')
     // ##get all tasks
-    const get_tasks = function () {
-        $http({
-            method: 'GET',
-            url: '/api/tasks'
-        })
-    }.then((function (res) {
-        $scope.tasks = res.data;
-		console.log("TCL: get_tasks -> res.data", res.data)
-    }, function (err) {
-        console.log('err', err);
-    }))
-    //get all taks and loop them into the ui
-    get_tasks()
-    //add a task
-    add_task = function () {
-        $http({
-            method: POST,
-            url: '/api/tasks',
-            data: {
-                name: task.name
-            }
-        })
-    }.then((function (res) {
-        $scope.task.push(res.task)
-        console.log("add_task -> res.task", res.task)
-        $scope.task = ''
-        return $scope.task
-    }, function (err) {
-        console.log('err', err);
-    }))
+   
+		$http({
+			method: 'GET',
+			url: '/api/tasks'
+		}).then(function(res) {
+			$scope.tasks = res.data
+		}, function(err) {
+			console.log('err', err)
+		})
+	
 
-    // selected_task
-    $scope.select_task = function(task) {
-        $scope.selected_task = task
-        console.log("TCL: $scope.select_task -> task", task)
-	}
+    // //add a task
+    // add_task = function () {
+    //     $http({
+    //         method: POST,
+    //         url: '/api/tasks/',
+    //         data: {
+    //             "name": $scope.task.name;
+    //         }
+    //     })
+    // }.then(function (res) {
+    //       res.status(200)
+    // }), function(err) {
+	// 		console.log('err', err)
+    // }
+    
+
+    // // selected_task
+    // $scope.select_task = function(task) {
+    //     $scope.selected_task = task
+    //     console.log("TCL: $scope.select_task -> task", task)
+	// }
 
 
-    // delete_task
-    $scope.delete_task = function () {
-        $http({
-            method: 'DELETE',
-            url: '/api/tasks/' + $scope.selected_task._id
-        }).then(function (res) {
-            console.log("$scope.selected_task._id", $scope.selected_task._id);
-            get_tasks()
-            console.log("TCL: $scope.delete_task -> $Scope.selected_task", $Scope.selected_task)
-            res.status(200)
-        }, function (err) {
-            console.log('err', err)
-        })
-    }
-    //complete a task
-    $scope.complete_task = function (selected_task) {
-        $http({
-            method: 'POST',
-            url: '/api/tasks/',
-            data: {
-                _id: task._id
-            }
-        }).then(function (req) {
-            $scope.tasks.filter(function (tasks) {
-                get_tasks()
-				console.log("TCL: $scope.complete_task -> get_tasks()", get_tasks())
-                return req.tasks
-				console.log("TCL: $scope.complete_task -> req.tasks", req.tasks)
-            }).then(function (err) {
-                console.log('err', err);
-            })
+    // // delete_task
+    // $scope.delete_task = function () {
+    //     $http({
+    //         method: 'DELETE',
+    //         url: '/api/tasks/' + $scope.selected_task._id
+    //     }).then(function (res) {
+    //         console.log("$scope.selected_task._id", $scope.selected_task._id);
+    //         get_tasks()
+    //         console.log("TCL: $scope.delete_task -> $Scope.selected_task", $Scope.selected_task)
+    //         res.status(200)
+    //     }, function (err) {
+    //         console.log('err', err)
+    //     })
+    // }
+    // //complete a task
+    // $scope.complete_task = function (selected_task) {
+    //     $http({
+    //         method: 'POST',
+    //         url: '/api/tasks/',
+    //         data: {
+    //             _id: task._id
+    //         }
+    //     }).then(function (req) {
+    //         $scope.tasks.filter(function (tasks) {
+    //             get_tasks()
+	// 			console.log("TCL: $scope.complete_task -> get_tasks()", get_tasks())
+    //             return req.tasks
+	// 			console.log("TCL: $scope.complete_task -> req.tasks", req.tasks)
+    //         }).then(function (err) {
+    //             console.log('err', err);
+    //         })
 
-        })
-    }    
+    //     })
+    // }    
 
 }])
